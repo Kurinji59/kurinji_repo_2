a52e4b4507fad9cd726ea15ef0c15fa9981dd059
Kurinji59 <98665172+Kurinji59@users.noreply.github.com>
2024-02-09 12:02:24 +1030

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,7 +1,8 @@
 git branch branch2
 git checkout branch2
 touch file4
-git commit -am "add file4"
-echo "file changed" >>file4
+git add .
+git commit -m "add file4"
+echo "file changed" > file4
 git stash
 git checkout main
